4e57e8cc317caefef5ff456e825aa354cf792d64
Kovy <codingstudent2021@gmail.com>
2025-02-21 20:48:33 +0200

    Adding strings directory

---
+++ csharp-strings/CSharpStrings.cs
@@ -0,0 +1,28 @@
+// /* C# Strings */
+// /*
+// 1. Strings are one of the most fundamental data types in programming. They represent sequences of characters and are used for storing and manipulating text.
+// 2. In C#, the string type is an immutable reference type, meaning once a string is created, it cannot be changed. This immutability has implications for performance and 
+// memory management, making it essential to understand how to work with strings efficiently.
+
+
+// */
+
+
+// using System
+
+// namespace Csharp
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             string firstName = "James" // example of a variable with a string data type assigned
+//         Console.WriteLine(firstName)
+//         }
+//     }
+
+// }
+
+
+
+// // Considerations; String Builder   using System.Text; | StringBuilder sb = new StringBuilder(); (creating an instance of string builder)
---
+++ csharp-strings/Program.cs
@@ -0,0 +1,38 @@
+﻿/* C# Strings */
+/*
+1. Strings are one of the most fundamental data types in programming. They represent sequences of characters and are used for storing and manipulating text.
+2. In C#, the string type is an immutable reference type, meaning once a string is created, it cannot be changed. This immutability has implications for performance and 
+memory management, making it essential to understand how to work with strings efficiently.
+
+
+*/
+
+
+using System;
+
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare a string variable and assign a name to it
+            string firstName = "James";
+
+            // String Method: Convert the string to uppercase and print it to the console
+            Console.WriteLine($"Upper case string: {firstName.ToUpper()}");
+
+
+            // String Method: Convert the string to lowercase and print it to the console
+            Console.WriteLine($"Lower case string: {firstName.ToLower()}");
+        }
+    }
+
+}
+
+// Considerations:
+// StringBuilder is used for efficient string manipulation.
+// Example:
+// using System.Text;
+// StringBuilder sb = new StringBuilder();  // Creating an instance of StringBuilder
