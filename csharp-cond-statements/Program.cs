87df3c10b36ba59ace04ace7fa8036b355062380
Kovy <codingstudent2021@gmail.com>
2025-03-21 21:36:14 +0200

    Adding Conditional Statement directory

---
+++ csharp-cond-statements/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+If-else Statements:
+The if statement evaluates a condition, and if it's true, the associated code block is executed. If the condition is false, the else block (if present) runs instead.
+
+SYNTAX:
+
+if (condition)
+{
+    // Code to execute if the condition is true
+}
+else
+{
+    // Code to execute if the condition is false
+}
+*/
+
+using System;
+
+namespace Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 20;
+            if (age >= 18)
+            {
+                Console.WriteLine("You are an adult.");
+            }
+            else
+            {
+                Console.WriteLine("You are a minor.");
+            }
+
+        }
+    }
+}
+
